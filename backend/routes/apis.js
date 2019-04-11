3ad4ac052551a8aee862c1a0688903f5821a107e
Joey Pinhas <jpinhas97@gmail.com>
2019-04-11 17:54:17 -0400

    Add basic route files

---
+++ backend/routes/apis.js
@@ -0,0 +1,12 @@
+const router  = require('express').Router();
+const multer  = require('multer');
+const model   = require('../models/api.js');
+
+const upload  = multer({ dest: 'uploads/' });
+
+function sendAsJSON(req, res) {
+  res.json(res.data);
+}
+
+router.route('/identify')
+  .post('/profile', upload.single('avatar'), model.processImage, sendAsJSON);
