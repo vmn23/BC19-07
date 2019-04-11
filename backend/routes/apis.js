01636071ddcd743d8c9ac930fe27da78cb5432ed
Joey Pinhas <jpinhas97@gmail.com>
2019-04-11 18:11:20 -0400

    Add a health route

--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -8,5 +8,15 @@ function sendAsJSON(req, res) {
   res.json(res.data);
 }
 
+function healthCheck(req, res, next) {
+  res.data = {
+    status: 200,
+  };
+}
+
 router.route('/identify')
   .post('/profile', upload.single('avatar'), model.processImage, sendAsJSON);
+
+router.route('/health', healthCheck, sendAsJSON);
+
+module.exports = router;
