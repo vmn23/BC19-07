4053e23753de3bc185d63c5b23f913e3420454c9
vmn23 <vmn23@cornell.edu>
2019-04-11 23:53:35 -0400

    List is able to take inputs

--- frontend/lib/list.dart
+++ frontend/lib/list.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
 
-class List extends StatelessWidget {
+import './star.dart';
+
+class List extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ListState();
+}
+
+class _ListState extends State<List> {
+  final symptomFilter = TextEditingController();
+  int rating = 5;
+  int price = 0;
+
+  void dispose() {
+    symptomFilter.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 229, 229, 229),
+      appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('Take a look at your options')),
       body: Center(
-        child: Text('You are at the list')
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: symptomFilter,
+                decoration: new InputDecoration(
+                  labelText: "Filter by symptoms",
+                  fillColor: Colors.white,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(5.0),
+                    borderSide: new BorderSide(),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                style: new TextStyle(
+                  color: Colors.black87,
+                  fontFamily: "Poppins",
+                ),
+              ),
+              Row(children: <Widget>[
+                Text('Rating ≥ ', style: new TextStyle(color: Colors.black87, fontFamily: "Poppins", fontSize: 16),),
+                StarRating(
+                  rating: rating,
+                  onRatingChanged: (rating) =>
+                      setState(() => this.rating = rating),
+                ),
+                Text('         '),
+                Text('Price ≤ ', style: new TextStyle(color: Colors.black87, fontFamily: "Poppins", fontSize: 16),),
+                StarRating(
+                  rating: price,
+                  star: false,
+                  onRatingChanged: (price) =>
+                      setState(() => this.price = price),
+                ),
+              ]),
+              Text(symptomFilter.text),
+              Text(rating.toString()),
+              Text(price.toString()),
+            ],
+          ),
+        ),
       ),
     );
   }
--- frontend/lib/picture.dart
+++ frontend/lib/picture.dart
@@ -18,9 +18,15 @@ class _TakePictureState extends State<TakePicture> {
     });
   }
 
+  void sendImage() {
+    // Navigate to the second screen using a named route
+    Navigator.pushNamed(context, '/list');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 229, 229, 229),
       appBar: AppBar(
         title: Text('Take a picture of the shelves in front of you'),
         backgroundColor: Colors.blueGrey[900],
@@ -32,10 +38,7 @@ class _TakePictureState extends State<TakePicture> {
                 Image.file(_image, height: 555),
                 RaisedButton(
                   child: Text('Analyze Image'),
-                  onPressed: () {
-                    // Navigate to the second screen using a named route
-                    Navigator.pushNamed(context, '/list');
-                  },
+                  onPressed: sendImage,
                 )
               ]),
       ),
